b4c44ae1c3ac19c10d08c2fb558dad252f4c548e
gbataille <g_bataille@hotmail.com>
2025-01-07 21:30:59 -0500

    comment token2

--- src/redux/userSlice.js
+++ src/redux/userSlice.js
@@ -15,7 +15,7 @@ const userSlice = createSlice({
     setUser: (state, action) => {
       state.user = action.payload.user;
       //state.token = action.payload.token;
-      localStorage.setItem('token', action.payload.token); // Store token in localStorage
+      //localStorage.setItem('token', action.payload.token); // Store token in localStorage
       localStorage.setItem('user', JSON.stringify(action.payload.user));
     },
     setLoading: (state, action) => {
